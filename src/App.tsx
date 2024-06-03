71883a04e0caf12a69bbec224fe62a6e52357f09
KevinHoffmannLU <contact@hoffmannkevin.com>
2024-06-03 17:26:05 +0200

    testing out some colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Kevin Hoffmann</h1>
+      <div className="test">
+        <button>React</button>
+        <button>Sass</button>
+        <button>Radix</button>
+        <button>Next.js</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
