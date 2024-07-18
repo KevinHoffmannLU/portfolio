7aacd11c3ebd675e0a38998381adbd0444fc0849
KevinHoffmannLU <contact@hoffmannkevin.com>
2024-07-18 14:08:01 +0200

    adding radiux ui icons

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,11 +1,11 @@
 export default function Intro() {
   return (
     <section className="flex justify-center gap-[10rem]">
-      <div className="flex flex-wrap items-start w-[80rem]">
+      <div className="flex flex-wrap items-start w-[80rem] ">
         <p className="pt-[11rem] text-[5rem] opacity-50">Hey there! I'm</p>
         <h1 className="ml-[1.2rem] text-[15rem]">Kevin</h1>
         <p className="text-[6rem]">
-          A <span className="text-[#a4dddb]">Web Developer</span> based out of
+          A <span className="text-[#a4dddb] ">Web Developer</span> based out of
           Luxembourg
         </p>
       </div>
